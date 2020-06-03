20e5cafafe1928f2cdbde3fe3617cd67ef1da2ad
relfto <66303314+relfto@users.noreply.github.com>
2020-06-03 08:45:09 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count=0;
+        val rb=findViewById<Button>(R.id.bt)
+        val rr=findViewById<TextView>(R.id.tx)
+        rb.setOnClickListener{
+            count++;
+            rr.text=count.toString();
+    
     }
 }
